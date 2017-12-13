70da29a4da91c3526d3a76eeabfa0294155dc2fb
tom <tanglu418@163.com>
2017-12-14 00:27:26 +0800

    首次提交

---
+++ app/src/main/java/tl/com/soilapp/MainActivity.java
@@ -0,0 +1,342 @@
+package tl.com.soilapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,UploadUtil.OnUploadProcessListener {
+
+    private TextView mTextMessage;
+    private Button location_btn;
+    private Button network_btn;
+    private Button best_btn;
+    private boolean flag;
+    private static Context context;
+
+    private static final String TAG = "uploadImage";
+
+    /**
+     * 去上传文件
+     */
+    protected static final int TO_UPLOAD_FILE = 1;
+    /**
+     * 上传文件响应
+     */
+    protected static final int UPLOAD_FILE_DONE = 2;  //
+    /**
+     * 选择文件
+     */
+    public static final int TO_SELECT_PHOTO = 3;
+    /**
+     * 上传初始化
+     */
+    private static final int UPLOAD_INIT_PROCESS = 4;
+    /**
+     * 上传中
+     */
+    private static final int UPLOAD_IN_PROCESS = 5;
+    /***
+     * 这里的这个URL是我服务器的javaEE环境URL
+     */
+    private static String requestURL = "http://120.79.51.73:8756/mgr/upload";
+    private Button selectButton,uploadButton;
+    private ImageView imageView;
+    private TextView uploadImageResult;
+    private ProgressBar progressBar;
+
+    private String picPath = null;
+    private ProgressDialog progressDialog;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        context = getApplicationContext();
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        location_btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initPermission();//针对6.0以上版本做权限适配
+    }
+
+    private void initView() {
+        location_btn = (Button) findViewById(R.id.location_btn);
+        network_btn = (Button) findViewById(R.id.net);
+        best_btn = (Button) findViewById(R.id.best);
+
+        //拍照需要的
+        selectButton = (Button) this.findViewById(R.id.selectImage);
+        uploadButton = (Button) this.findViewById(R.id.uploadImage);
+        selectButton.setOnClickListener(this);
+        uploadButton.setOnClickListener(this);
+        imageView = (ImageView) this.findViewById(R.id.imageView);
+        uploadImageResult = (TextView) findViewById(R.id.uploadImageResult);
+        progressDialog = new ProgressDialog(this);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+    }
+
+    private void initPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //检查权限
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //请求权限
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                flag = true;
+            }
+        } else {
+            flag = true;
+        }
+    }
+
+    /**
+     * 权限的结果回调函数
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            flag = grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.location_btn:
+                if (flag) {
+                    getGPSLocation();
+                } else {
+                    Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.net:
+                if (flag) {
+                    getNetworkLocation();
+                } else {
+                    Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.best:
+                if (flag) {
+                    getBestLocation();
+                } else {
+                    Toast.makeText(this, "no permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.selectImage:
+                Intent intent = new Intent(this,SelectPicActivity.class);
+                startActivityForResult(intent, TO_SELECT_PHOTO);
+                break;
+            case R.id.uploadImage:
+                if(picPath!=null)
+                {
+                    handler.sendEmptyMessage(TO_UPLOAD_FILE);
+                }else{
+                    Toast.makeText(this, "上传的文件路径出错", Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 通过GPS获取定位信息
+     */
+    public void getGPSLocation() {
+        Location gps = LocationUtils.getGPSLocation(this);
+        if (gps == null) {
+            //设置定位监听，因为GPS定位，第一次进来可能获取不到，通过设置监听，可以在有效的时间范围内获取定位信息
+            LocationUtils.addLocationListener(context, LocationManager.GPS_PROVIDER, new LocationUtils.ILocationListener() {
+                @Override
+                public void onSuccessLocation(Location location) {
+                    if (location != null) {
+                        Toast.makeText(MainActivity.this, "gps onSuccessLocation location:  lat==" + location.getLatitude() + "     lng==" + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "gps location is null", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "gps location: lat==" + gps.getLatitude() + "  lng==" + gps.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 通过网络等获取定位信息
+     */
+    private void getNetworkLocation() {
+        Location net = LocationUtils.getNetWorkLocation(this);
+        if (net == null) {
+            Toast.makeText(this, "net location is null", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "network location: lat==" + net.getLatitude() + "  lng==" + net.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 采用最好的方式获取定位信息
+     */
+    private void getBestLocation() {
+        Criteria c = new Criteria();//Criteria类是设置定位的标准信息（系统会根据你的要求，匹配最适合你的定位供应商），一个定位的辅助信息的类
+        c.setPowerRequirement(Criteria.POWER_LOW);//设置低耗电
+        c.setAltitudeRequired(true);//设置需要海拔
+        c.setBearingAccuracy(Criteria.ACCURACY_COARSE);//设置COARSE精度标准
+        c.setAccuracy(Criteria.ACCURACY_LOW);//设置低精度
+        //... Criteria 还有其他属性，就不一一介绍了
+        Location best = LocationUtils.getBestLocation(this, c);
+        if (best == null) {
+            Toast.makeText(this, " best location is null", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "best location: lat==" + best.getLatitude() + " lng==" + best.getLongitude(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode== Activity.RESULT_OK && requestCode == TO_SELECT_PHOTO)
+        {
+            picPath = data.getStringExtra(SelectPicActivity.KEY_PHOTO_PATH);
+            Log.i(TAG, "最终选择的图片="+picPath);
+            Bitmap bm = BitmapFactory.decodeFile(picPath);
+            imageView.setImageBitmap(bm);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    /**
+     * 上传服务器响应回调
+     */
+    @Override
+    public void onUploadDone(int responseCode, String message) {
+        progressDialog.dismiss();
+        Message msg = Message.obtain();
+        msg.what = UPLOAD_FILE_DONE;
+        msg.arg1 = responseCode;
+        msg.obj = message;
+        handler.sendMessage(msg);
+    }
+
+    private void toUploadFile()
+    {
+        uploadImageResult.setText("正在上传中...");
+        progressDialog.setMessage("正在上传文件...");
+        progressDialog.show();
+        String fileKey = "pic";
+        UploadUtil uploadUtil = UploadUtil.getInstance();;
+        uploadUtil.setOnUploadProcessListener(this);  //设置监听器监听上传状态
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("orderId", "11111");
+        uploadUtil.uploadFile( picPath,fileKey, requestURL,params);
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case TO_UPLOAD_FILE:
+                    toUploadFile();
+                    break;
+
+                case UPLOAD_INIT_PROCESS:
+                    progressBar.setMax(msg.arg1);
+                    break;
+                case UPLOAD_IN_PROCESS:
+                    progressBar.setProgress(msg.arg1);
+                    break;
+                case UPLOAD_FILE_DONE:
+                    String result = "响应码："+msg.arg1+"\n响应信息："+msg.obj+"\n耗时："+UploadUtil.getRequestTime()+"秒";
+                    uploadImageResult.setText(result);
+                    break;
+                default:
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+
+    };
+
+    @Override
+    public void onUploadProcess(int uploadSize) {
+        Message msg = Message.obtain();
+        msg.what = UPLOAD_IN_PROCESS;
+        msg.arg1 = uploadSize;
+        handler.sendMessage(msg );
+    }
+
+    @Override
+    public void initUpload(int fileSize) {
+        Message msg = Message.obtain();
+        msg.what = UPLOAD_INIT_PROCESS;
+        msg.arg1 = fileSize;
+        handler.sendMessage(msg );
+    }
+}
