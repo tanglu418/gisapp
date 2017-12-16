7cbad99c9c8bf088df118bb7b973c4da72170f61
tom <tanglu418@163.com>
2017-12-17 00:23:19 +0800

    定位的图片移动存在问题，保存一次。

---
+++ app/src/main/java/tl/com/soilapp/SendHttp.java
@@ -0,0 +1,8 @@
+package tl.com.soilapp;
+
+/**
+ * Created by Administrator on 2017/12/15 0015.
+ */
+
+public class SendHttp {
+}
---
+++ app/src/main/java/tl/com/soilapp/ToastUtil.java
@@ -0,0 +1,8 @@
+package tl.com.soilapp;
+
+/**
+ * Created by Administrator on 2017/12/16 0016.
+ */
+
+public class ToastUtil {
+}
